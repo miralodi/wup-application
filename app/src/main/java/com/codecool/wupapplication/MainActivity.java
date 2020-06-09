89b6fcdce47d965a4ef843d0726ffbbca0be76f0
Lorya <mircsi.lodi@gmail.com>
2020-06-09 13:10:09 +0200

    Request cards in MainActivity currently without mvp architecture

--- app/src/main/java/com/codecool/wupapplication/MainActivity.java
+++ app/src/main/java/com/codecool/wupapplication/MainActivity.java
@@ -3,22 +3,39 @@ package com.codecool.wupapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.codecool.wupapplication.model.Card;
+import com.codecool.wupapplication.model.ResponseCards;
+import com.codecool.wupapplication.network.BaseApiService;
+import com.codecool.wupapplication.network.UtilsApi;
+import com.codecool.wupapplication.util.DateFormatter;
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView navView;
+    BaseApiService mApiService;
+    List<Card> cardList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mApiService = UtilsApi.getAPIService();
+        requestCards();
 
         navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -43,4 +60,49 @@ public class MainActivity extends AppCompatActivity {
         statementsBadge.setNumber(getResources().getInteger(R.integer.badge_number));
         statementsBadge.setBackgroundColor(getResources().getColor(R.color.orange));
     }
+
+    private void requestCards() {
+        mApiService.requestCards()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Observer<List<ResponseCards>>() {
+                @Override
+                public void onSubscribe(Disposable d) {
+
+                }
+
+                @Override
+                public void onNext(List<ResponseCards> responseCards) {
+                    for (ResponseCards resCard : responseCards) {
+                        cardList.add(new Card(
+                                resCard.getCardId(),
+                                resCard.getCardNumber(),
+                                resCard.getCardHolderName(),
+                                resCard.getCurrency(),
+                                resCard.getAvailableBalance(),
+                                resCard.getCurrentBalance(),
+                                resCard.getMinPayment(),
+                                DateFormatter.formatDateString(resCard.getDueDate()),
+                                resCard.getReservations(),
+                                resCard.getBalanceCarriedOverFromLastStatement(),
+                                resCard.getSpendingsSinceLastStatement(),
+                                DateFormatter.formatDateString(resCard.getYourLastRepayment()),
+                                resCard.getAccountDetails(),
+                                resCard.getCardImage()
+                        ));
+                    }
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    System.out.println(e.getMessage());
+                }
+
+                @Override
+                public void onComplete() {
+                    System.out.println("SUCCESS");
+                }
+            });
+    }
+
 }
