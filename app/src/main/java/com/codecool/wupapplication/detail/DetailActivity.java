4d19a6430cfaea793d81047b0fddee8e933dd3ba
Lorya <mircsi.lodi@gmail.com>
2020-06-12 19:45:04 +0200

    Fill details page with data

--- app/src/main/java/com/codecool/wupapplication/detail/DetailActivity.java
+++ app/src/main/java/com/codecool/wupapplication/detail/DetailActivity.java
@@ -1,6 +1,5 @@
 package com.codecool.wupapplication.detail;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -18,6 +17,7 @@ import com.codecool.wupapplication.util.CurrencyFormatter;
 public class DetailActivity extends AppCompatActivity {
 
     private Card currentCard;
+    String currency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +35,9 @@ public class DetailActivity extends AppCompatActivity {
         initializeViews();
     }
 
-
-
     private void initializeViews() {
-        String currency = currentCard.getCurrency();
+        currency = currentCard.getCurrency();
+
         final int currentBalance = currentCard.getCurrentBalance();
         final int availableBalance = currentCard.getAvailableBalance();
         final int reservations = currentCard.getReservations();
@@ -66,5 +65,29 @@ public class DetailActivity extends AppCompatActivity {
         TextView mReservationValue = findViewById(R.id.reservations_value);
         mReservationCurrency.setText(currency);
         mReservationValue.setText(CurrencyFormatter.formatIntToCurrency(reservations));
+
+        DetailItem balance = findViewById(R.id.carried_balance);
+        balance.setCurrencyText(currency);
+        balance.setValueText(CurrencyFormatter.formatIntToCurrency(currentCard.getBalanceCarriedOverFromLastStatement()));
+
+        DetailItem totalSpendings = findViewById(R.id.total_spendings);
+        totalSpendings.setCurrencyText(currency);
+        totalSpendings.setValueText(CurrencyFormatter.formatIntToCurrency(currentCard.getSpendingsSinceLastStatement()));
+
+        DetailItem lastRepayment = findViewById(R.id.last_repayment);
+        lastRepayment.setValueText(currentCard.getYourLastRepayment());
+
+        DetailItem accountLimit = findViewById(R.id.account_limit);
+        accountLimit.setCurrencyText(currency);
+        accountLimit.setValueText(CurrencyFormatter.formatIntToCurrency(currentCard.getAccountDetails().getAccountLimit()));
+
+        DetailItem accountNumber = findViewById(R.id.account_number);
+        accountNumber.setValueText(currentCard.getAccountDetails().getAccountNumber());
+
+        DetailItem cardNumber = findViewById(R.id.card_number);
+        cardNumber.setValueText(currentCard.getCardNumber());
+
+        DetailItem cardHolder = findViewById(R.id.card_holder_name);
+        cardHolder.setValueText(currentCard.getCardHolderName());
     }
 }
